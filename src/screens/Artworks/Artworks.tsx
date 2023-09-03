0c89d875a0280034a5026dd965cb7600bd0e699d
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-03 18:33:36 -0300

    Connect Artworks with selector

--- src/screens/Artworks/Artworks.tsx
+++ src/screens/Artworks/Artworks.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Text, View } from "react-native";
 import { useSelector } from "react-redux";
 import ArtworksList from "../../components/ArtworksList/ArtworksList";
+import { getArtworks } from "../../store/selectors/artworksSelectors";
 
 const Artworks = () => {
-  const artworks = useSelector((state) => state?.artworks?.artworks);
-
+  const artworks = useSelector(getArtworks);
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{ flex: 1, padding: 20 }}>
       <Text>Artworks Screen PAGE</Text>
       <ArtworksList data={artworks} />
     </View>
@@ -15,4 +15,3 @@ const Artworks = () => {
 }
 
 export default Artworks;
-  
