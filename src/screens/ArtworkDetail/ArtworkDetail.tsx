935c190537cc2091a09ca0b1eaa0224d3c064e19
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-03 15:52:19 -0300

    Add ArtworkDetail Screen

---
+++ src/screens/ArtworkDetail/ArtworkDetail.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ArtworkDetail = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>ArtworkDetail Screen PAGE</Text>
+    </View>
+  );
+}
+
+export default ArtworkDetail;
+  
