5b08a544501e1dc5592c3d967593f33f3f31d7ec
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-03 15:53:00 -0300

    Add FavoirtesArtworksStack

--- src/screens/FavoritesArtworks/FavoritesArtworks.tsx
+++ src/screens/FavoritesArtworks/FavoritesArtworks.tsx
@@ -1,10 +1,13 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, View } from "react-native";
+import { ScreenNames } from "../ScreenNames";
 
 const FavoritesArtworks = () => {
+  const navigation = useNavigation();
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>FavoritesArtworks Screen PAGE</Text>
+        <Text onPress={() => navigation.navigate(ScreenNames.ArtworkDetail)}>FavoritesArtworks Screen PAGE</Text>
       </View>
     );
 }
