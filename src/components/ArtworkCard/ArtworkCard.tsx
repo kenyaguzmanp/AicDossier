80a5eb711a9f7f781ddaf3e26e78907e911e3aff
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-04 11:06:18 -0300

    Add ArtworkCard

---
+++ src/components/ArtworkCard/ArtworkCard.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import { Card, Text } from 'react-native-paper';
+import { getThumbnailImageUrl } from '../../utils/artworkDetails';
+import { TouchableOpacity } from 'react-native';
+import FavoriteIcon from '../FavoriteIcon/FavoriteIcon';
+import { useDispatch, useSelector } from 'react-redux';
+import { getIsFavoriteArtwork } from '../../store/selectors/artworksSelectors';
+import { addArtworkToFavorites, removeArtworkToFavorites } from '../../store/slices/artworksSlice';
+
+
+const ArtworkCard = ({ artwork  }) => {
+
+  const isFavorite = useSelector(getIsFavoriteArtwork(artwork.id));
+
+  const dispatch = useDispatch();
+
+  const handleOnPressFavorite = () => {
+    if (isFavorite) {
+      dispatch(removeArtworkToFavorites(artwork))
+    } else {
+      dispatch(addArtworkToFavorites(artwork))
+    }
+  }
+
+  const renderFavoriteIcon = () => {
+    return (
+      <TouchableOpacity onPress={handleOnPressFavorite}>
+        <FavoriteIcon isSelected={isFavorite} />
+      </TouchableOpacity>
+    )
+  }
+  
+  const RightContent = () => renderFavoriteIcon();
+
+  return (
+    <Card>
+      <Card.Title title={artwork.title} subtitle={artwork.artist_title} right={RightContent} />
+      <Card.Cover source={{ uri: getThumbnailImageUrl(artwork.image_id) }} />
+      {artwork?.thumbnail?.alt_text && (
+        <Card.Content>
+          {/* <Text variant="titleLarge">{artwork.title}</Text> */}
+          <Text variant="bodyMedium">{artwork.thumbnail.alt_text}</Text>
+        </Card.Content>
+      )}
+    </Card>
+  );
+}
+
+export default ArtworkCard;
