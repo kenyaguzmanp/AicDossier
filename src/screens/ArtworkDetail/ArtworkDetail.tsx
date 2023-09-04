9e235a15ec159df5fafd5c8ec2dae2d38c58c9e3
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-04 18:07:25 -0300

    Modify ArtworkDetail

--- src/screens/ArtworkDetail/ArtworkDetail.tsx
+++ src/screens/ArtworkDetail/ArtworkDetail.tsx
@@ -1,25 +1,56 @@
 import React, { useEffect, useState } from "react";
-import { Dimensions, Image, ScrollView, View } from "react-native";
+import { Image, ScrollView, View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
-import { getSelectedArtwork } from "../../store/selectors/artworksSelectors";
+import { getHasErrorArtworkDetail, getIsLoadingArtworkDetail, getSelectedArtwork } from "../../store/selectors/artworksSelectors";
 import { getFullImageUrl } from "../../utils/artworkDetails";
 import ImageModal from "react-native-image-modal";
 import Chips from "../../components/Chips/Chips";
 import { selectedArtwork } from "../../store/slices/artworksSlice";
 import ArtworkDescription from "../../components/ArtworkDescription/ArtworkDescription";
+import { WIDTH } from "../../theme";
+import RetryDetail from "../../components/RetryDetail/RetryDetail";
+import { enhancedFetchArtworkDetail } from "../../api/services";
+import Spinner from "../../Spinner/Spinner";
 
-const WIDTH = Dimensions.get("window").width;
-
-const ArtworkDetail = () => {
+const ArtworkDetail = (props) => {
   const currentArtwork = useSelector(getSelectedArtwork);
+  const isLoadingArtworkDetail = useSelector(getIsLoadingArtworkDetail);
+  const hasErrorArtworkDetail = useSelector(getHasErrorArtworkDetail);
   const [isFullImageLoaded, setIsFullImageLoaded] = useState(false);
+  
   const dispatch = useDispatch();
+
+  const onRetry = () => {
+    if (props?.route?.params?.artworkId) {
+      enhancedFetchArtworkDetail({
+        serviceParams: {
+          artworkId: props.route.params.artworkId,
+        },
+      });
+    }
+  }
 
   useEffect(() => {
     return () => {
       dispatch(selectedArtwork(null));
     }
   }, [])
+
+  if (isLoadingArtworkDetail) {
+    return (
+      <View style={{ justifyContent: "center", alignSelf: "center", paddingVertical: 50 }}>
+        <Spinner size="large" />
+      </View>
+    );
+  }
+
+  if (!isLoadingArtworkDetail &&  hasErrorArtworkDetail) {
+    return (
+      <>
+        <RetryDetail onRetry={onRetry}/>
+      </>
+    )
+  }
 
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
