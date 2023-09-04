3818a20d96190752d31f2f31b286d791e7b48473
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-03 23:04:29 -0300

    Add translatioon in BottomTab

--- src/components/BottomTabs/BottomTabs.tsx
+++ src/components/BottomTabs/BottomTabs.tsx
@@ -1,18 +1,17 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../../screens/Home/Home';
-import { ScreenNames } from '../../screens/ScreenNames';
-// import FavoritesArtworks from '../../screens/FavoritesArtworks/FavoritesArtworks';
 import ArtworksStack from '../../navigation/ArtworksStack';
 import FavoritesArtworksStack from '../../navigation/FavoritesArtworksStack';
+import i18n from '../../i18n/locales';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name={ScreenNames.Home} component={Home} />
-      <Tab.Screen name={ScreenNames.Artworks} component={ArtworksStack} options={{ headerShown: false }} />
-      <Tab.Screen name={ScreenNames.FavoritesArtworks} component={FavoritesArtworksStack} options={{ headerShown: false }} />
+      <Tab.Screen name={i18n.t("HOME")} component={Home} />
+      <Tab.Screen name={i18n.t("EXPLORE")} component={ArtworksStack} options={{ headerShown: false }} />
+      <Tab.Screen name={i18n.t("FAVORITES")} component={FavoritesArtworksStack} options={{ headerShown: false }} />
     </Tab.Navigator>
   );
 };
