2e459d10899636d5c3ba92ba2faf948cdd436bda
Kenya Guzman <kenyaguzmanp@gmail.com>
2023-09-05 01:10:26 -0300

    Fix issue whith navigation

--- src/components/ArtworkThumbnail/ArtworkThumbnail.tsx
+++ src/components/ArtworkThumbnail/ArtworkThumbnail.tsx
@@ -3,19 +3,12 @@ import { useNavigation } from '@react-navigation/native';
 import { TouchableOpacity, View } from 'react-native';
 import { ScreenNames } from '../../screens/ScreenNames';
 import { Artwork } from '../../interfaces';
-import { enhancedFetchArtworkDetail } from '../../api/services';
 import ArtworkCard from '../ArtworkCard/ArtworkCard';
 
 const ArtworkThumbnail = ({ artwork }: { artwork: Artwork }): JSX.Element => {
   const navigation = useNavigation();
 
   const onPressArtwork = () => {
-    enhancedFetchArtworkDetail({
-      serviceParams: {
-        artworkId: artwork.id,
-        fakeService: false,
-      },
-    });
     navigation.navigate(ScreenNames.ArtworkDetail, { artworkId: artwork.id });
   }
 
--- src/screens/ArtworkDetail/ArtworkDetail.tsx
+++ src/screens/ArtworkDetail/ArtworkDetail.tsx
@@ -1,24 +1,24 @@
 import React, { useEffect, useState } from "react";
 import { Image, ScrollView, View } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { getHasErrorArtworkDetail, getIsLoadingArtworkDetail, getSelectedArtwork } from "../../store/selectors/artworksSelectors";
+import { useSelector } from "react-redux";
+import { getHasErrorArtworkDetail, getIsFavoriteArtwork, getSelectedArtwork } from "../../store/selectors/artworksSelectors";
 import { getFullImageUrl } from "../../utils/artworkDetails";
 import ImageModal from "react-native-image-modal";
 import Chips from "../../components/Chips/Chips";
-import { selectedArtwork } from "../../store/slices/artworksSlice";
 import ArtworkDescription from "../../components/ArtworkDescription/ArtworkDescription";
 import { WIDTH } from "../../theme";
 import RetryDetail from "../../components/RetryDetail/RetryDetail";
 import { enhancedFetchArtworkDetail } from "../../api/services";
 import Spinner from "../../Spinner/Spinner";
+import StarIcon from "../../components/StarIcon/StarIcon";
 
 const ArtworkDetail = (props) => {
   const currentArtwork = useSelector(getSelectedArtwork);
-  const isLoadingArtworkDetail = useSelector(getIsLoadingArtworkDetail);
   const hasErrorArtworkDetail = useSelector(getHasErrorArtworkDetail);
+  const isFavorite = useSelector(getIsFavoriteArtwork(props?.route?.params?.artworkId));
   const [isFullImageLoaded, setIsFullImageLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   
-  const dispatch = useDispatch();
 
   const onRetry = () => {
     if (props?.route?.params?.artworkId) {
@@ -26,17 +26,22 @@ const ArtworkDetail = (props) => {
         serviceParams: {
           artworkId: props.route.params.artworkId,
         },
+        setLoadingState: setIsLoading,
       });
     }
   }
 
   useEffect(() => {
-    return () => {
-      dispatch(selectedArtwork(null));
-    }
+    enhancedFetchArtworkDetail({
+      serviceParams: {
+        artworkId: props.route.params.artworkId,
+        fakeService: false,
+      },
+      setLoadingState: setIsLoading,
+    });
   }, [])
 
-  if (isLoadingArtworkDetail) {
+  if (isLoading) {
     return (
       <View style={{ justifyContent: "center", alignSelf: "center", paddingVertical: 50 }}>
         <Spinner size="large" />
@@ -44,7 +49,7 @@ const ArtworkDetail = (props) => {
     );
   }
 
-  if (!isLoadingArtworkDetail &&  hasErrorArtworkDetail) {
+  if (!isLoading &&  hasErrorArtworkDetail) {
     return (
       <>
         <RetryDetail onRetry={onRetry}/>
@@ -55,37 +60,37 @@ const ArtworkDetail = (props) => {
   return (
     <ScrollView style={{ flex: 1, backgroundColor: "white" }}>
       <View style={{ flex: 1 }}>
-        {currentArtwork?.thumbnail && (
-          <View style={{ position: isFullImageLoaded ? "absolute" : "relative", width: '100%', left: 0 }}>
-            <Image
-              style={{
-                width: WIDTH,
-                height: WIDTH,
-              }}
-              source={{
-                uri: currentArtwork.thumbnail.lqip,
-              }}
-            />
-          </View>
-        )}
-        <ImageModal
-          resizeMode="contain"
-          imageBackgroundColor="#000000"
-          style={{
-            width: isFullImageLoaded ? WIDTH : 0,
-            height: WIDTH,
-          }}
-          source={{
-            uri: getFullImageUrl(currentArtwork.image_id),
-          }}
-          onLoadStart={() => setIsFullImageLoaded(false)}
-          onLoadEnd={() => {
-            setIsFullImageLoaded(true)
-            // setTimeout(() => {
-            //   setIsFullImageLoaded(true)
-            // }, 5000);
-          }}
-        />
+        <View>
+          {currentArtwork?.thumbnail && (
+            <View style={{ position: isFullImageLoaded ? "absolute" : "relative", width: '100%', left: 0 }}>
+              <Image
+                style={{
+                  width: WIDTH,
+                  height: WIDTH,
+                }}
+                source={{
+                  uri: currentArtwork.thumbnail.lqip,
+                }}
+              />
+            </View>
+          )}
+          <ImageModal
+            resizeMode="contain"
+            imageBackgroundColor="#000000"
+            style={{
+              width: isFullImageLoaded ? WIDTH : 0,
+              height: WIDTH,
+            }}
+            source={{
+              uri: getFullImageUrl(currentArtwork.image_id),
+            }}
+            onLoadStart={() => setIsFullImageLoaded(false)}
+            onLoadEnd={() => setIsFullImageLoaded(true)}
+          />
+          {isFavorite && (<View style={{ position: "absolute", right: 10, top: 10 }}>
+            <StarIcon width={50} height={50} />
+          </View>)}
+        </View>
       </View>
       <ArtworkDescription artwork={currentArtwork} />
       {currentArtwork.category_titles && <Chips items={currentArtwork.category_titles} />}
